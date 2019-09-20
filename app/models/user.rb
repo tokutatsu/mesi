ef90b0c4555fce2d759f5ac87cf03fea0920440a
tokutatsu <s17t248@stu.kagawa-u.ac.jp>
2019-09-20 15:47:35 +0900

    コールバック時の問題を解決した #11

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
+         :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: %i[google_oauth2]
 
   def self.from_omniauth(auth)
