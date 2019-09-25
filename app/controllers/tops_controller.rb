5553db6fdc63ef34b395887c7be568d830354821
hirany <devhirany@gmail.com>
2019-09-25 23:31:13 +0900

    make tops and welcomes controller

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,6 +1,4 @@
 class TopsController < ApplicationController
-  before_action :authenticate_user!, only: [:edit, :update, :destroy]
   def index
-    redirect_to '/index' unless user_signed_in?
+    redirect_to '/resource' unless user_signed_in?
   end
-end
