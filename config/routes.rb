591e194dab2d380f4a1b5eb95e4d3957d2a29cc3
hirany <devhirany@gmail.com>
2019-09-26 22:20:45 +0900

    change link_to of index.html.slim

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root 'tops#index'
-  # resource '/index', to: 'welcomes#index'
   resources :welcomes
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks'
