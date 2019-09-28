99a4e2d883de09f33463f2bf615b413f349f114a
hirany <devhirany@gmail.com>
2019-09-28 14:40:09 +0900

    ログアウト機能を実装した

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root 'tops#index'
   resources :welcomes
+  # delete 'logout/:id', to: 'sessions#destroy'
+  devise_scope :user do
+    get '/users/sign_out', to: 'devise/sessions#destroy'
+  end
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks'
   }
